127f8ea67ace35944dac00e53a0bf4b31e90503a
Franko <frankovn@hotmail.co.uk>
2022-10-06 16:54:57 +0200

    Made the profile page or started with it.

--- eduspace/src/pages/Profile.jsx
+++ eduspace/src/pages/Profile.jsx
@@ -11,20 +11,20 @@ function Profile() {
        <img src={process.env.PUBLIC_URL + '/logo192.png'}width="200" />
        <p></p>
 
-    Name:<input id="textfield" name="textfield" type="text" placeholder="Company/Sponsor" />
+    Name: <input id="textfield" name="textfield" type="text" placeholder="Enter Name" />
     <p></p>
-UserName:<input id="textfield" name="textfield" type="text" placeholder="Email" />
+UserName: <input id="textfield" name="textfield" type="text" placeholder="Username" />
     <p></p>
-    Email:<input id="textfield" name="textfield" type="text" placeholder="Password" />
+    Email: <input id="textfield" name="textfield" type="text" placeholder="Email" />
     <p></p> 
     {/*USED TO MAKE A TEXT OX WITH WORDS WRITTEN IN IT*/}
-    Bio:<input id="textfield" name="textfield" type="text" placeholder="Password" />
+    Bio: <input id="textfield" name="textfield" type="text" placeholder="Bio" />
     <p></p> 
-    Degree:<input id="textfield" name="textfield" type="text" placeholder="Password" />
+    Degree: <input id="textfield" name="textfield" type="text" placeholder="Degree" />
     <p></p> 
-    Faculty:<input id="textfield" name="textfield" type="text" placeholder="Password" />
+    Faculty: <input id="textfield" name="textfield" type="text" placeholder="Faculty" />
     <p></p> 
-    Tutoring Modules:<input id="textfield" name="textfield" type="text" placeholder="Password" />
+    Tutoring Modules: <input id="textfield" name="textfield" type="text" placeholder="Modules tutoring" />
     <p></p> 
     <button className='button2'>Save</button>
 
