1d65c4b1594d8487a89e3c00cb4d85a78fb4cadb
Deeds42 <114473950+Deeds42@users.noreply.github.com>
2022-10-20 12:31:59 +0200

    man uploaded

--- eduspace/src/contexts/CartContext.js
+++ eduspace/src/contexts/CartContext.js
@@ -1,16 +1,4 @@
-import React, {useState} from 'react';
-
-import { database } from '../lib/dataList';
+import {createContext, useState} from 'react';
 
 
-export const CartContext = React.createContext(); 
- 
-export const CartProvider = (props) => { 
- const [cart, setItem] = React.useState(database); 
-
-  return ( 
-    <CartContext.Provider value={[cart, setItem]}> 
-      {props.children} 
-    </CartContext.Provider> 
-  ) 
-}
+export const CartContext = createContext(null); 
